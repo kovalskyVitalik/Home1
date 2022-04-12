02d68f20eb64155bef96db43511e5b67c39414cd
kovalskyVitalik <91958823+kovalskyVitalik@users.noreply.github.com>
2022-04-12 11:58:46 +0300

    Add files via upload

---
+++ HomeWorkApp.java
@@ -0,0 +1,9 @@
+public class HomeWorkApp {
+
+    public static void main (String[] args) {
+        System.out.print("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+}
