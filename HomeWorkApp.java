6257def58db10e6771301c7bcea1926b16406d76
kovalskyVitalik <91958823+kovalskyVitalik@users.noreply.github.com>
2022-04-12 19:23:36 +0300

    Delete HomeWorkApp.java

--- HomeWorkApp.java
+++
@@ -1,9 +0,0 @@
-public class HomeWorkApp {
-
-    public static void main (String[] args) {
-        System.out.print("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
-    }
-
-}
